5466366068898021d6bf73d86e47604550bf25a9
brian <brian.rivera.proboste@gmail.com>
2020-09-18 22:53:14 -0300

    estructura del formulario y lista de nuevas habilidades reutilizable

---
+++ src/components/personajes/FormLista.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { ItemFormLista } from "./ItemFormLista";
+import Swal from 'sweetalert2';
+import { useForm } from "../../hooks/useForm";
+
+export const FormLista = ({ title = "", Form = [], items = [] }) => {
+    
+  const [itemsState, setitemsState] = useState(items);
+  const [values, handleInputChange, reset] = useForm();
+  
+  const handleAddHabilitie = (e) => {
+    e.preventDefault();
+        console.log(values);
+        if(!values.name || !values.description ) return Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Por favor lleve el campo nombre y descripcion para agregar una habilidad'
+          });
+        if((itemsState.find(e=>e.name === values.name))) return Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Habilidad repetida'
+          });
+          reset();
+        return setitemsState([values,...itemsState]); 
+
+  };
+
+
+
+  const handleDeleteItem = (name) => {
+    setitemsState(itemsState.filter((e)=>e.name!==name));
+  }
+
+
+
+  return (
+    <>
+      <h5>{title}</h5>
+
+      {Form &&
+        Form.map((r, index) => {
+          switch (r.container) {
+            case "input":
+              return (
+                <div key={index.toString()+'i'} className="col-sm-12">
+                  {r.name}
+                  <input type="text" name={r.name} className="inputForm" onChange={handleInputChange} value={(values[r.name]) ? values[r.name]: ''}/>
+                </div>
+              );
+            case "textarea":
+              return (
+                <div key={index.toString()+'t'} className="col-sm-12">
+                  {r.name}
+                  <textarea
+                    name={r.name}
+                    cols="30"
+                    rows="10"
+                    className="inputForm"
+                    onChange={handleInputChange}
+                    value={(values[r.name]) ? values[r.name]: ''}
+                  ></textarea>
+                </div>
+              );
+            case "select":
+              return (
+                <div className="col-sm-12">
+                  <select name={r.name} className="inputForm" onChange={handleInputChange}>
+                    {r.options.map((op, i) => (
+                      <option value={(values[op.name]) ? values[op.name]: ''}></option>
+                    ))}
+                  </select>
+                </div>
+              );
+            default:
+              break;
+          }
+        })}
+      <div className="col-sm-12">
+        <button onClick={handleAddHabilitie}>Agregar habilidad</button>
+      </div>
+      <div className="col-sm-12">
+        <hr />
+        <h5>Lista {title}</h5>
+        <hr />
+
+        <div className="listPoderes">
+          {itemsState.length > 0 ? (
+            itemsState.map((r, i) => (
+              <ItemFormLista
+                key={i + "hab"}
+                name={r.name}
+                description={r.description}
+                handleDeleteItem={handleDeleteItem}
+              ></ItemFormLista>
+            ))
+          ) : (
+            <p>sin habilidades</p>
+          )}
+        </div>
+        <br />
+      </div>
+    </>
+  );
+};
--- src/components/personajes/Personajes.jsx
+++ src/components/personajes/Personajes.jsx
@@ -1,65 +1,59 @@
 import React from "react";
 import "./../styles.scss";
+import { FormLista } from "./FormLista";
 
 export const Personajes = () => {
-
- 
+  const formHabilidades = [
+    {
+      name: "name",
+      container: "input",
+      type: "text",
+    },
+    {
+      name: "description",
+      container: "textarea",
+      type: "text",
+    },
+  ];
 
   return (
     <div className="container container_app">
       <div className="row base_container">
         <h3>Ingrese personaje</h3>
         <div className="col-sm-6">
-            <hr/>
+          <hr />
           <h5>Datos personaje</h5>
           <form>
-              Nombre
-              <input type="text" name="" className="inputForm" value=""/>
-              Raza
-              <select name="" id="" className="inputForm">
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-              </select>
-
-              <div className="row">
-                  <h5>Habilidades</h5>
-                  <div className="col-sm-12">
-                    Nombre
-                    <input type="text" name="" className="inputForm" value=""/>
-                  </div>
-
-                  <div className="col-sm-12">
-                  Descripcion
-                    <textarea name="" id="" cols="30" rows="10"  className="inputForm"></textarea>
-                  </div>
-                  <div className="col-sm-12 listPoderes">
-                      <hr/>
-                      <h5>Lista habilidades</h5>
-                      <hr/>
-
-                      <ul className="list-group-item list-group-item-action">
-                        <div className="d-flex w-100 justify-content-between">
-                          <h6 className="mb-1">List group item heading</h6>
-                          <small>x</small>
-                        </div>
-                        <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-
-                      </ul>
-                      <br/>
-                    </div>
-              </div>
-              
+            Nombre
+            <input type="text" name="" className="inputForm" value="" />
+            Raza
+            <select name="" id="" className="inputForm">
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+            </select>
+            <div className="row newSkillsContainer">
+              <FormLista
+                title="Habilidades"
+                Form={formHabilidades}
+                items={[]}
+              ></FormLista>
+            </div>
+            <button>Guardar</button>
           </form>
+          <br />
         </div>
         <div className="col-sm-6">
-            <hr/>
+          <hr />
           <h5>Imagen</h5>
-          <br/>
+          <br />
           <input type="file" className="form-control" />
-          <img src="https://www.alfabetajuega.com/wp-content/uploads/2019/08/dragon-ball-goku.jpg" className="img-fluid" alt="Responsive image"></img>
-            <br/>
-            <br/>
+          <img
+            src="https://www.alfabetajuega.com/wp-content/uploads/2019/08/dragon-ball-goku.jpg"
+            className="img-fluid"
+          ></img>
+          <br />
+          <br />
         </div>
       </div>
     </div>
